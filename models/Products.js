51f65f98004a4aa978dc3bcb8ceaaa5f855626bb
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-06-23 21:48:00 -0500

    products exist but is not connected, TODO work on functionality

---
+++ models/Products.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Schema
+const MenuSchema = Schema({
+    category: {type: String, enum: ['Desayuno', 'Normal']},
+    img: String,//url de imagen
+    price: {type:Number, default: 0},
+    product: String, //nombre de la comida
+    //options: Array, extras: Array
+})
+
+//exports module
+module.exports = mongoose.model('Menu', MenuSchema);
---
+++ routes/products.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//models
+const Product = require('../models/Products');
