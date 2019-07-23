5fb52dbaad4a68a61c476aee80d5ae7c3979ed55
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-07-23 08:32:57 -0500

    config changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.port = process.argv[2] || process.env.PORT || 8080;
-exports.mongoUrl = process.env.MONGO_URI || 'mongodb+srv://bquser:elpass1@burgerback-hj09v.mongodb.net/test?retryWrites=true&w=majority';
+exports.mongoUrl = process.env.MONGO_URI;
 exports.secret = process.env.JWT_SECRET || 'xxxxxxxx'; // JWT secret
 exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@localhost';
 exports.adminPassword = process.env.ADMIN_PASSWORD || 'changeme';
